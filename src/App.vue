0d7811649d309d77a6020d45116c5b88f3b71931
凌寒舞 <2273376391@qq.com>
2022-10-29 19:34:28 +0800

    初始化-路由+elementPlus

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="app">
+    <div id="nav">
+      <router-link to="/home" active-class="hanwu-active">Home</router-link> |
+      <router-link to="/about" active-class="hanwu-active">About</router-link>
+    </div>
+    <router-view />
+  </div>
+</template>
+
+<style lang="less" scoped>
+
+</style>
